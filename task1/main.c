1c3fca26517d5a7d4f598f1a7d26ac90d07a9a0f
greedywolf11 <13711879829@139.com>
2024-10-21 23:49:33 +0800

    task1

--- task1/main.c
+++ task1/main.c
@@ -1,8 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+double f(double y)
+{return -y;
+}
+void eular(double x0,double y0,double h,double error)
+{
+double x=x0;
+double y=y0;
+double y_next;
+printf("x\t\ty\n");
+printf("%.6f\t%.6f\n",x,y);
+while (1)
+{y_next=y+h*f(y);
+x=x+h;
+printf("%.6f\t%.6f\n",x,y);
+if(fabs(y_next-y)<error)
+{printf("已达到定态值\n");
+printf("近似定态值: y=%.6f",y_next);
+break;}
+y=y_next;
+	
+}
 
-int main(int argc, char *argv[]) {
-	printf("Hello World!\n");
+
+
+
+
+}
+
+int main() 
+{double x0=0.0000000;
+double y0;
+double a;
+double h=0.001;
+double error=0.000001;
+printf("请输入a的值:");
+scanf("%lf",&a);
+y0=a;
+
+eular(x0,y0,h,error);
+	
 	return 0;
 }
 
